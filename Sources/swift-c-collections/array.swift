ff87125231f12965c51bb800c8e764b8010549bb
Fang Ling <alice@jxx.world>
2023-03-09 08:20:15 +0800

    (Array) add more wrap functions & test

--- Sources/swift-c-collections/array.swift
+++ Sources/swift-c-collections/array.swift
@@ -16,13 +16,81 @@ public struct Array<T> {
         }
     }
     
+    public var is_empty : Bool {
+        get {
+            return array.pointee.is_empty
+        }
+    }
+    
     public init() {
         array = array_init(Int(MemoryLayout<T>.size))
+    }
+    
+    /* It's caller's responsibility to free the array. */
+    public func `deinit`() {
+        array_deinit(array)
     }
     
     public func append(_ new_element : T) {
         var new_element = new_element
         array_append(array, &new_element)
+    }
+    
+    public func insert(_ new_element : T, at i : Int) {
+        var new_element = new_element
+        array_insert(array, i, &new_element)
+    }
+    
+    public func swap_at(_ i : Int, _ j : Int) {
+        array_swap_at(array, i, j)
+    }
+    
+    public subscript(index : Int) -> T {
+        get {
+            return array_get(array, index)
+                .bindMemory(to: T.self,
+                            capacity: MemoryLayout<T>.size).pointee
+        }
+        set(new_element) {
+            var new_element = new_element
+            array_set(array, index, &new_element)
+        }
+    }
+    
+    public func remove(at index : Int) -> T {
+        let buf_ptr = array_remove(array, index)
+            .bindMemory(to: T.self, capacity: MemoryLayout<T>.size)
+        let buf = buf_ptr.pointee
+        free(buf_ptr)
+        return buf
+    }
+    
+    public func remove(at index : Int) {
+        array_removen(array, index)
+    }
+    
+    public func remove_last() -> T {
+        let buf_ptr = array_remove_last(array)
+            .bindMemory(to: T.self, capacity: MemoryLayout<T>.size)
+        let buf = buf_ptr.pointee
+        free(buf_ptr)
+        return buf
+    }
+    
+    public func remove_last() {
+        array_remove_lastn(array)
+    }
+    
+    public func remove_first() -> T {
+        let buf_ptr = array_remove_first(array)
+            .bindMemory(to: T.self, capacity: MemoryLayout<T>.size)
+        let buf = buf_ptr.pointee
+        free(buf_ptr)
+        return buf
+    }
+    
+    public func remove_first() {
+        array_remove_firstn(array)
     }
     
     public func to_swift_array() -> [T] {
--- Tests/swift-c-collectionsTests/swift_c_collectionsTests.swift
+++ Tests/swift-c-collectionsTests/swift_c_collectionsTests.swift
@@ -1,6 +1,21 @@
 import XCTest
-@testable import swift_c_collections
+@testable import CCollections
 
 final class swift_c_collectionsTests: XCTestCase {
-    func testExample() throws {}
+    func testExample() {
+        var array = CCollections.Array<Int>()
+        
+        array.append(19358)
+        array.append(12333)
+        
+        XCTAssertEqual(array[0], 19358)
+        XCTAssertEqual(array[1], 12333)
+        array[1] = 12321
+        XCTAssertEqual(array[1], 12321)
+        
+        array.swap_at(0, 1)
+        XCTAssertEqual(array.to_swift_array(), [12321, 19358])
+        
+        array.deinit()
+    }
 }
